9e3635a4ab4dc1ede848db6625f39111d3eeabad
Nguyen Nam <nghoofnam@gmail.com>
2025-07-24 19:05:48 +0700

    add: game*

---
+++ source/game/game.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include "../core/grid.h"
+#include "../data/blocks.cpp"
+
+class Game
+{
+public:
+    Game();
+    ~Game();
+    void Draw();
+    void HandleInput();
+    void MoveBlockDown();
+    bool gameOver;
+    int score;
+    Music music;
+
+private:
+    void MoveBlockLeft();
+    void MoveBlockRight();
+    Block GetRandomBlock();
+    std::vector<Block> GetAllBlocks();
+    bool IsBlockOutside();
+    void RotateBlock();
+    void LockBlock();
+    bool BlockFits();
+    void Reset();
+    void UpdateScore(int linesCleared, int moveDownPoints);
+    Grid grid;
+    std::vector<Block> blocks;
+    Block currentBlock;
+    Block nextBlock;
+    Sound rotateSound;
+    Sound clearSound;
+};
