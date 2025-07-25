19989a016e794ce0fd2588523a36374891eb3374
Nguyen Nam <nghoofnam@gmail.com>
2025-07-25 19:44:20 +0700

    add: game*

--- source/game/game.h
+++ source/game/game.h
@@ -1,34 +1,45 @@
 ﻿#pragma once
 #include "../core/grid.h"
-#include "../data/blocks.cpp"
+#include "../data/block.h"
+#include "../blocks/L_block.h"
+#include "../blocks/J_block.h"
+#include "../blocks/I_block.h"
+#include "../blocks/O_block.h"
+#include "../blocks/S_block.h"
+#include "../blocks/T_block.h"
+#include "../blocks/Z_block.h"
+#include "../audio/audio_service.h"
+#include <memory>
 
 class Game
 {
 public:
     Game();
     ~Game();
+
     void Draw();
     void HandleInput();
     void MoveBlockDown();
     bool gameOver;
     int score;
-    Music music;
+    std::shared_ptr<AudioService> audioService;
+    std::shared_ptr<AudioService> GetAudioService();
 
 private:
     void MoveBlockLeft();
     void MoveBlockRight();
-    Block GetRandomBlock();
-    std::vector<Block> GetAllBlocks();
+    std::unique_ptr<Block> GetRandomBlock();
+    std::vector<std::unique_ptr<Block>> GetAllBlocks();
     bool IsBlockOutside();
     void RotateBlock();
     void LockBlock();
     bool BlockFits();
     void Reset();
     void UpdateScore(int linesCleared, int moveDownPoints);
+
     Grid grid;
-    std::vector<Block> blocks;
-    Block currentBlock;
-    Block nextBlock;
-    Sound rotateSound;
-    Sound clearSound;
+    std::vector<std::unique_ptr<Block>> blocks;
+    std::unique_ptr<Block> currentBlock;
+    std::unique_ptr<Block> nextBlock;
+
 };
