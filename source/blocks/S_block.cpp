f9433f8fc5ed3bad9a47b747de762b220f0c335a
Nguyen Nam <nghoofnam@gmail.com>
2025-07-25 19:43:50 +0700

    add: SBlock

---
+++ source/blocks/S_block.cpp
@@ -0,0 +1,16 @@
+#include "S_block.h"
+
+SBlock::SBlock()
+{
+    id = 5;
+    cells[0] = { Position(0, 1), Position(0, 2), Position(1, 0), Position(1, 1) };
+    cells[1] = { Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 2) };
+    cells[2] = { Position(1, 1), Position(1, 2), Position(2, 0), Position(2, 1) };
+    cells[3] = { Position(0, 0), Position(1, 0), Position(1, 1), Position(2, 1) };
+    Move(0, 3);
+}
+
+std::unique_ptr<SBlock> SBlock::Create()
+{
+    return std::make_unique<SBlock>();
+}
